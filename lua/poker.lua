fc19fb1d5f173dd5a03dd8249d9d88716d54b3cf
Satoki <43191933+satokiy@users.noreply.github.com>
2025-02-11 15:49:10 +0900

    json decode

--- lua/poker.lua
+++ lua/poker.lua
@@ -1,7 +1,36 @@
+local cjson = require "cjson"
+
 local function evaluate_hand(hand)
     -- ポーカーの役判定ロジックをここに実装
     return "役の名前"
 end
 
-local hand = {"AS", "KS", "QS", "JS", "10S"} -- 例としてロイヤルフラッシュ
-ngx.say(evaluate_hand(hand))
+local function handle_request()
+    ngx.req.read_body()
+    local body = ngx.req.get_body_data()
+
+    if not body then
+        ngx.status = ngx.HTTP_BAD_REQUEST
+        ngx.say("No body found")
+        return
+    end
+
+    local result, res = pcall(cjson.decode, body)
+    if result == false then
+        ngx.status = ngx.HTTP_BAD_REQUEST
+        ngx.say("Invalid JSON")
+        return
+    end
+    
+    if not res.hand then
+        ngx.status = ngx.HTTP_BAD_REQUEST
+        ngx.say("Invalid Request. Missing 'hand' key")
+        return
+    end
+
+    local hand = res.hand
+    local result = evaluate_hand(hand)
+    ngx.say(cjson.encode({result = result}))
+end
+
+handle_request()
